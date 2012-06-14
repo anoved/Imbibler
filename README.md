a3714b8d4ca3de9e618c285d1db46ffcdba13177
Jim DeVona <jim.devona@gmail.com>
2012-06-13 20:57:26 -0400

    Rewritten to search by verse class identifiers instead of paragraphs.

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
 Imbibler
 ========
 
-This Ruby script reads the index of a Bible from [Bible Gateway](http://www.biblegateway.com/) and generates a comma-separated value (CSV) table of the Bible content in `Book,Chapter,Verse,Text` form. The name is a play on the idea that the script "imbibes" the Bible (and then, um, pukes it back up in tabular form).
+This Ruby script reads the index of a Bible from [Bible Gateway](http://www.biblegateway.com/) and generates a comma-separated value (CSV) table of the Bible content in `Book,Chapter,Verse,Text` form. The name is a play on the idea that the script "imbibes" the Bible (and then, um, regurgitates it in tabular form).
 
-Note that this script is intended for personal reading and research purposes. Its output should not be distributed, as doing so may constitute [copyright violation](http://www.biblegateway.com/versions/New-International-Version-NIV-Bible/#copy). (Wait, wait - are these God's words, or some publisher's? Hmm?)
+This script is intended for personal reading and research purposes only. Its output should not be distributed, as doing so may constitute [copyright violation](http://www.biblegateway.com/versions/New-International-Version-NIV-Bible/#copy).
 
 Usage
 -----
 
-	imbibler.rb OUTPUTFILE INDEXURL
+	imbibler.rb OUTPUT [INDEX]
 
-The script expects two command-line arguments. The first, `OUTPUTFILE`, will be used as the filename for the output CSV table. The second, `INDEXURL`, is the index URL of a Bible Gateway Bible, such as [http://www.biblegateway.com/versions/New-International-Version-NIV-Bible/](http://www.biblegateway.com/versions/New-International-Version-NIV-Bible/). (In general, URLs should be quoted on the command line to avoid misinterpretation of any punctuation characters they may contain.) Links to all Bible version indices are [available here](http://www.biblegateway.com/versions/).
+The script expects one or two command-line arguments. The first, `OUTPUT`, will be used as the filename for the output CSV table. The second, `INDEX`, is the index URL of a Bible Gateway Bible, such as [http://www.biblegateway.com/versions/New-International-Version-NIV-Bible/](http://www.biblegateway.com/versions/New-International-Version-NIV-Bible/), which is the default. (In general, URLs should be quoted on the command line to avoid misinterpretation of any punctuation characters they may contain.) Links to all Bible version indices are [available here](http://www.biblegateway.com/versions/).
 
-Note that the character encoding of the output file is UTF-8. If given the option, select this encoding when importing the CSV table into other applications to ensure that punctuation and accented characters are represented accurately.
+The script also dumps the text of each chapter to text files in the current working directory. Verse text files are titled in the pattern `BB BOOK CCC.txt`, where `BB` is the book number, `BOOK` is the book title, and `CCC` is the chapter number.
+
+Note that the character encoding of output files is UTF-8. If given the option, select this encoding when importing output into other applications to ensure that punctuation marks and accented characters are represented correctly.
 
 Prerequisites
 -------------
@@ -20,17 +22,3 @@ Prerequisites
 Imbibler relies on [Nokogiri](http://nokogiri.org/) for easy HTML parsing. Nokogiri is not part of the Ruby standard library, but is easily installed with:
 
 	sudo gem install nokogiri
-
-To-do
------
-
-- Error handling!
-- Annotation handling. Footnotes and cross-references are currently discarded. Conceivably it might be desirable to embed these in the associated verse text or alternatively to save them to a separate file.
-- Caching. Pages could be saved when downloaded so that subsequent runs could utilize local copies for much faster processing.
-- Configurable throttle. Currently Imbibler pauses 1 second between page downloads to avoid any rate limits that may interfere with site scraping. Bible Gateway may not apply any such restraints, in which case the pause could be reduced or removed for faster processing.
-- Easier interface. Imbibler currently expects the URL of a Bible Gateway Bible index page as one of its arguments. The list of indices could easily be stored within the script, allowing the user to simply specify the name, abbreviation, or some unique substring of either.
-
-Credits
--------
-
-Thanks to Geoffrey Dagley's [bible_gateway](https://github.com/gdagley/bible_gateway) verse-lookup code for an example of reading Bible Gateway with Ruby and Nokogiri.
--- imbibler.rb
+++ imbibler.rb
@@ -1,113 +1,154 @@
 #!/usr/bin/env ruby
 
-#
-# Nokogiri provides convenient HTML parsing abilities.
-# install with "sudo gem install nokogiri"
-# http://nokogiri.org
-#
 require 'nokogiri'
 require 'open-uri'
 require 'csv'
 
-
-def ConvertChapter(book, chapter, url, csv)
-
-	# Download and parse the chapter page
+#
+# ReadBookChapter
+#
+# Processes a BibleGateway.com bible chapter page, which contains verse text
+# in a variety of tags (typically but not always p). Each verse is comprised
+# of one or more spans tagged with a unique verse class identifier. Find each
+# one, concatenate the verse text, output it in tabular form, and also output
+# a text file containing the verse content of this chapter alone.
+#
+# Arguments:
+#  book, the title of the book containing this chapter
+#  bookNum, the number of the book containing this chapter
+#  url, the URL of page containing the text of this chapter
+#  output, stream to write output to
+#
+def ReadBookChapter(book, bookNum, url, output)
+	
+	# No verses have been read yet.
+	chapterVerses = []
+	
+	# Read and parse the chapter page.
 	page = Nokogiri::HTML(open(URI.escape(url)))
 	
-	# Find the part of the page that contains the actual bible text
-	text = page.at('div.result-text-style-normal')
+	# Find the first [or any] verse, just so we can figure out how the verses
+	# of this chapter are tagged. (Abbreviation formats vary, so don't guess.)
+	firstVerse = page.at('p span.text')
+	firstVerseClass = firstVerse['class'].split(' ')[1]
+	parts = firstVerseClass.scan(/(.+?)-(.+?)-(.+?)/)[0]
+	bookCode = parts[0]
+	chapterNum = parts[1]
+	verseNum = parts[2]
 	
-	# Look for the start of each verse in the text
-	text.search('sup.versenum').each do |verse|
+	while true
 		
-		# The content of the sup.versnum node is simply the verse number
-		verseNumber = verse.content
+		# Assemble tag used to find all the elements that comprise this verse.
+		# (Initially identical to firstVerseClass, but we'll soon increment verseNum)
+		verseClass = format("%s-%s-%s", bookCode, chapterNum, verseNum)
 		
-		# Assemble the verse text from all the following nodes up until we run
-		# into the next verse number; ignore things like footnotes and headers.
-		verseText = ""		
-		verseNode = verse.next_sibling
-		while verseNode != nil
-			if verseNode.text?
-				# This node is text; just append its contents to verse text
-				verseText += verseNode.content
-			elsif verseNode.elem?
-				# This node is some sort of tag. Is it friend or foe?
-				if verseNode.name() == "sup"
-					if verseNode.get_attribute("class") == "versenum"
-						# Aha - next verse starts here. Stop assembling this one.
-						break
-					end
-				elsif verseNode.name() == "p"
-					# We represent paragraph tags as two newlines
-					verseText += "\n\n"
-				elsif verseNode.name() == "br"
-					# We represent linebreak tags as one newline
-					verseText += "\n"
-				end
-			end
-			verseNode = verseNode.next_sibling			
+		# Search the page for all elements tagged with verseClass (maybe >1).
+		## Technically speaking, CSS identifiers should not start with numerals,
+		## so verse class tags for books starting with a digit (like "1Sam" for
+		## "1 Samuel") violate this convention. As a result, Nokogiri's CSS-style
+		## search method fails, so we use an Xpath selector instead.
+		## verseParts = page.search("p span.#{verseClass}")
+		verseParts = page.search("//p//span[@class='text #{verseClass}']|//table//span[@class='text #{verseClass}']")
+		
+		# Call it quits for this chapter if no matches for this verseNum.
+		break if verseParts == nil or verseParts.empty?
+		
+		STDOUT << "#{verseNum} "
+		STDOUT.flush
+		
+		# Get list of text tagged as this verse, minus any extraneous notes, etc.
+		verseParagraphs = []
+		verseParts.each do |versePart|
+			versePart.search('span.chapternum,sup.versenum,sup.crossreference,sup.footnote').remove
+			verseParagraphs.push(versePart.content)
+			# .squeeze(' ').strip
 		end
 		
-		# Nokogiri auto-converts &nbsp; to pesky non-breaking space characters.
-		# Convert them to regular spaces. First quoted space is Unicode nbsp.
-		verseText.gsub!(" ", " ")
+		# Concatenate the parts of this verse,
+		verseText = verseParagraphs.join("\n\n")
 		
-		# Trim leading and trailing whitespace.
-		verseText.strip!
+		# add it to the list of verses in this particular chapter,
+		chapterVerses.push(verseText)
 		
-		# Output this record to the CSV table.
-		csv << [book, chapter, verseNumber, verseText]
+		# and write it out to the output table (along with book, chapter, and number).
+		output << [book, chapterNum, verseNum, verseText]
 		
+		# Increment the verse number and get ready to do it all over again!
+		verseNum.succ!	
 	end
 	
-end
-
-def ConvertIndex(url, csv)
+	STDOUT << "\n"
 	
-	# Download and parse the index page
-	index = Nokogiri::HTML(open(URI.escape(url)))
-	
-	# Find the part of the page containing the table of chapter links
-	table = index.at('table.infotable')
-	
-	# Look for chapter link "a" tag in the table
-	table.search('a').each do |link|
-		
-		# Read the href attribute of the link tag to the chapter page address
-		path = link.get_attribute('href')
-		url = 'http://www.biblegateway.com' + path
-		
-		# Read the title attribute and split on the last space to get the book and chapter
-		title = link.get_attribute('title')
-		split = title.rindex(' ')
-		book = title[0..split-1]
-		chapter = title[split+1..-1]
-		
-		# Go go gadget read this chapter
-		puts "Reading " + title + "..."
-		ConvertChapter(book, chapter, url, csv)
-		
-		# Let's be polite and pause a bit between pulling down each page
-		sleep 1
-		
+	# Dump the concatenated contents of chapterVerses to a text file.
+	chapterFilename = format("%02d %s %03d.txt", bookNum, book, chapterNum)
+	File.open(chapterFilename, 'w') do |file|
+		file.puts chapterVerses.join("\n")
 	end
-	
+
 end
 
+#
+# ReadIndex
+#
+# Processes a BibleGateway.com bible index, which is essentially a list of links
+# to individual bible chapter pages. Identifies each chapter link in the index
+# and hands it off to ReadBookChapter for verse extraction.
+#
+# Arguments:
+#  url, URL of the BibleGateway.com bible index to process
+#  output, stream to write output to
+#
+def ReadIndex(url, output)
 
-if ARGV.size != 2
-	STDERR.puts "usage: imbibler.rb OUTPUTFILE INDEXURL"
-	STDERR.puts "       (NIV Bible index URL: http://www.biblegateway.com/versions/New-International-Version-NIV-Bible/)"
+	# Book 1 is assumed to be titled "Genesis".
+	lastBookTitle = "Genesis"
+	bookNum = "1"
+	
+	# Read and parse the index page.
+	testament = Nokogiri::HTML(open(URI.escape(url)))
+	
+	# Just get the table of book/chapter links.
+	bookTable = testament.at('table.infotable')
+	
+	# Find each link in the table...
+	bookTable.search('a').each do |chapterLink|
+	
+		# This is the URL of the page for this chapter.
+		chapterUrl = 'http://www.biblegateway.com' + chapterLink['href']
+		
+		# Get the book title and chapter number associated with each link.
+		titleString = chapterLink['title']
+		splitChar = titleString.rindex(' ')
+		bookTitle = titleString[0..splitChar-1]
+		chapterNum = titleString[splitChar+1..-1]
+			
+		# Keep track of the book number (used just for titling chapter files).
+		bookNum.succ! if bookTitle != lastBookTitle
+		
+		# Get ReadBookChapter on the phone and tell him to read this chapter.
+		puts "Reading #{bookTitle} #{chapterNum}:"
+		ReadBookChapter(bookTitle, bookNum, chapterUrl, output)
+		lastBookTitle = bookTitle
+	end
+end
+
+# Print basic usage information and quit if we don't have one or two arguments.
+if ARGV.size < 1 or ARGV.size > 2
+	STDERR.puts "usage: imbibler.rb OUTPUT [INDEX]"
+	STDERR.puts "INDEX URL defaults to that of the NIV Bible:"
+	STDERR.puts "http://www.biblegateway.com/versions/New-International-Version-NIV-Bible/"
+	STDERR.puts "Text files containing the text of each chapter are output to the current"
+	STDERR.puts "working directory with names \"BB BOOK CCC.txt\" where BB is the book"
+	STDERR.puts "number, BOOK is the book title, and CCC is the chapter number."
 	exit 1
 end
 
-outputFile = ARGV[0]
-indexURL = ARGV[1]
+# Assign default index URL, but override it if an index URL is explicitly specified.
+indexUrl = "http://www.biblegateway.com/versions/New-International-Version-NIV-Bible/"
+indexUrl = ARGV[1] if ARGV.size == 2
 
-csv = CSV.open(outputFile, 'w')
-ConvertIndex(indexURL, csv)
+# Create a comma-separated value file for the output, and start processing the index.
+csv = CSV.open(ARGV[0], 'w')
+ReadIndex(indexUrl, csv)
 csv.close
 
-exit 0
